d420b18387575733afadcb355b780e4ea622c5d8
hippohwj <wenjiehu1993@gmail.com>
2023-11-01 19:48:06 -0500

    update util

--- annotate_run.py
+++ annotate_run.py
@@ -1,28 +1,24 @@
 import os
+import sys
+
 
 ROOT_DIR = os.path.join(os.path.dirname(__file__))
+if ROOT_DIR in sys.path:
+    sys.path.insert(0, ROOT_DIR)
 
-# Disable the TOKENIZERS_PARALLELISM
-TOKENIZER_FALSE = "export TOKENIZERS_PARALLELISM=false\n"
+command = (
+    f"export PYTHONPATH={ROOT_DIR}:$PYTHONPATH && "
+    f"export TOKENIZERS_PARALLELISM=false && "
+    f"python {ROOT_DIR}/scripts/annotate_binder_program.py --dataset wikitq "
+    f"--dataset_split test "
+    f"--prompt_file templates/prompts/wikitq_binder.txt "
+    f"--n_parallel_prompts 1 "
+    f"--max_generation_tokens 512 "
+    f"--temperature 0.4 "
+    f"--sampling_n 20 "
+    f"-v"
+)
 
-# wikitq nsql annotation command
-os.system(fr"""{TOKENIZER_FALSE}python {ROOT_DIR}/scripts/annotate_binder_program.py --dataset wikitq \
- --dataset_split test \
- --prompt_file templates/prompts/wikitq_binder.txt \
- --n_parallel_prompts 1 \
- --max_generation_tokens 512 \
- --temperature 0.4 \
- --sampling_n 20 \
- -v""")
+os.system(command)
 
-# tab_fact nsql annotation command
-os.system(fr"""{TOKENIZER_FALSE}python {ROOT_DIR}/scripts/annotate_binder_program.py --dataset tab_fact \
- --dataset_split test \
- --prompt_file templates/prompts/tab_fact_binder.txt \
- --n_parallel_prompts 1 \
- --n_processes 1 \
- --n_shots 18 \
- --max_generation_tokens 256 \
- --temperature 0.6 \
- --sampling_n 50 \
- -v""")
+
--- execute_run.py
+++ execute_run.py
@@ -1,27 +1,21 @@
 import os
+import sys
+
 
 ROOT_DIR = os.path.join(os.path.dirname(__file__))
+if ROOT_DIR in sys.path:
+    sys.path.insert(0, ROOT_DIR)
 
-# Disable the TOKENIZERS_PARALLELISM
-TOKENIZER_FALSE = "export TOKENIZERS_PARALLELISM=false\n"
+command = (
+    f"export PYTHONPATH={ROOT_DIR}:$PYTHONPATH && "
+    f"export TOKENIZERS_PARALLELISM=false && "
+    f"python {ROOT_DIR}/scripts/execute_binder_program.py --dataset wikitq "
+    f"--dataset_split test "
+    f"--qa_retrieve_pool_file templates/qa_retrieve_pool/qa_retrieve_pool.json "
+    f"--input_program_file binder_program_wikitq_test_chatgpt.json "
+    f"--output_program_execution_file binder_program_wikitq_test_exec.json "
+    f"--vote_method simple "
+)
 
-# wikitq nsql execution command
-os.system(fr"""{TOKENIZER_FALSE}python {ROOT_DIR}/scripts/execute_binder_program.py --dataset wikitq \
- --dataset_split test \
- --qa_retrieve_pool_file templates/qa_retrieve_pool/qa_retrieve_pool.json \
- --input_program_file binder_program_wikitq_test.json \
- --output_program_execution_file binder_program_wikitq_test_exec.json \
- --vote_method simple
- """)
+os.system(command)
 
-# tab_fact nsql execution command
-os.system(fr"""{TOKENIZER_FALSE}python {ROOT_DIR}/scripts/execute_binder_program.py --dataset tab_fact \
- --dataset_split test \
- --qa_retrieve_pool_file templates/qa_retrieve_pool/qa_retrieve_pool.json \
- --input_program_file binder_program_tab_fact_test.json \
- --output_program_execution_file binder_program_tab_fact_test_exec.json \
- --allow_none_and_empty_answer \
- --vote_method answer_biased \
- --answer_biased 1 \
- --answer_biased_weight 3 \
- """)
