17d3f3a4e1ff1a78112ef1027034c192b9067725
xxcisxxc <w5966466@163.com>
2023-12-09 13:37:05 -0600

    WikiTQ dataset

---
+++ final/dataset.py
@@ -0,0 +1,86 @@
+from urllib.request import urlretrieve
+import zipfile, os, pandas
+from typing import Iterator, Optional
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+def download_unzip(name: str, ex_dir: str, url: str) -> None:
+    if not os.path.exists(ex_dir):
+        print(f"Downloading dataset {name}")
+        urlretrieve(url, f"{name}.zip")
+        with zipfile.ZipFile(f"{name}.zip", "r") as zip_file:
+            zip_file.extractall()
+
+
+def sample_data(col: pandas.Series, sep: str = "|"):
+    sample_len = min(10, len(col))
+    samples = list(col.sample(n=sample_len))
+    str_samples = [str(s) for s in samples]
+    return sep.join(str_samples)
+
+
+na_values = ["n / a", "none", "-", "xx", "unknown", "—", "–", "--", "?"]
+
+pandas_template = """>>> df.name = NAME
+>>> df.dtype
+DTYPE
+"""
+
+
+def get_pandas_table(dfcsv: pandas.DataFrame) -> str:
+    table_title = dfcsv.name
+    dtypes = dfcsv.dtypes
+    print_dtypes = str(dtypes).split("\n")
+    str_dtype_list = [
+        f"{content}\t# e.g.,\t{sample_data(dfcsv[name])}"
+        for name, content in zip(dtypes.keys(), print_dtypes[:-1])
+    ]
+    str_dtype_list.append(print_dtypes[-1])
+    str_dtype = "\n".join(str_dtype_list)
+    table_schema = pandas_template.replace("NAME", table_title)
+    table_schema = table_schema.replace("DTYPE", str_dtype)
+    return dfcsv, table_schema
+
+
+# Create in-memory database
+engine = create_engine("sqlite://", echo=False)
+
+
+# Base class
+class Base(DeclarativeBase):
+    pass
+
+
+sqlalchemy_template = """class Table(Base):
+    __tablename__ = "NAME"
+    index: Mapped[int] = mapped_column(primary_key=True)
+    COLS
+"""
+col_template = "NAME: Mapped[TYPE] = mapped_column('ACTUAL') # e.g., EXPL"
+
+
+def get_sqlalchemy_table(dfcsv: pandas.DataFrame) -> str:
+    table_title = dfcsv.name
+    table_schema = sqlalchemy_template.replace("NAME", table_title)
+    dtypes = dfcsv.dtypes
+    cols = []
+    isnull_col = dfcsv.isnull().any()
+    for i, (name, dtype) in enumerate(dtypes.items()):
+        col = col_template.replace("NAME", f"col{i}")
+        if dtype == "int64":
+            col = col.replace("TYPE", "Optional[int]" if isnull_col[name] else "int")
+        elif dtype == "float64":
+            col = col.replace(
+                "TYPE", "Optional[float]" if isnull_col[name] else "float"
+            )
+        else:
+            col = col.replace("TYPE", "Optional[str]" if isnull_col[name] else "str")
+        col = col.replace("ACTUAL", name)
+        col = col.replace("EXPL", sample_data(dfcsv[name]))
+        cols.append(col)
+    table_schema = table_schema.replace("COLS", "\n    ".join(cols))
+    exec(table_schema)
+    Base.metadata.create_all(engine)
+    dfcsv.to_sql(table_title, engine, if_exists="append")
+    return table_schema
---
+++ final/wikitq.py
@@ -0,0 +1,89 @@
+import os, pandas, json, pprint
+from typing import Iterator, Optional
+
+from dataset import download_unzip, get_pandas_table, get_sqlalchemy_table, na_values
+
+# Download dataset
+wikitq_hash = "7d455a5a707b96341ef72aff9428749d443d8aa9"
+wikitq_dir = f"{os.getcwd()}/WikiTableQuestions-{wikitq_hash}"
+wikitq_url = f"https://github.com/ppasupat/WikiTableQuestions/archive/{wikitq_hash}.zip"
+download_unzip("wikitq", wikitq_dir, wikitq_url)
+
+
+data_dir = f"{wikitq_dir}/docs/viewer/csv"
+wikitq_json = f"{wikitq_dir}/docs/viewer/csv/tables.json"
+
+
+def get_pandas_schema(table_id: str, table_title: str) -> str:
+    dfcsv = pandas.read_html(
+        f"{data_dir}/{table_id}",
+        encoding="UTF-8",
+        na_values=na_values,
+    )[0]
+    dfcsv.name = table_title
+    return get_pandas_table(dfcsv)
+
+
+def get_sqlalchemy_schema(table_id: str, table_title: str) -> str:
+    dfcsv = pandas.read_html(
+        f"{data_dir}/{table_id}",
+        encoding="UTF-8",
+        na_values=na_values,
+    )[0]
+    dfcsv.name = table_title
+    return get_sqlalchemy_table(dfcsv)
+
+
+def get_table(use_sql: bool = False):
+    if use_sql:
+        return get_sqlalchemy_schema
+    else:
+        return get_pandas_schema
+
+
+def dataset_iter(
+    table_list: list[str] = [],
+    *,
+    use_sql: bool = False,
+) -> Iterator[tuple[Optional[pandas.DataFrame], str, list[str], list[str]]]:
+    """Iterator for Dataset wikitq
+
+    Args:
+        table_list (list[str], optional): The list of requested tables, where empty list means all the tables in the dataset. Defaults to [].
+        use_sql (bool, optional): Whether use sqlalchemy plan or pandas plan. Pandas plan will also returns a dataframe. Defaults to False.
+
+    Yields:
+        Iterator[tuple[Optional[pandas.DataFrame], str, list[str], list[str]]]: Return an iterator of [(data frame, optional), table prompt, a list of questions, a list of answers]
+    """
+    with open(wikitq_json, "r") as jsonfile:
+        json_data = json.load(jsonfile)["tables"]
+    if len(table_list) == 0:
+        for first_index in json_data:
+            for table_index in first_index[1]:
+                table_id = f"{first_index[0]}-csv/{table_index}-clean.html"
+                meta_qa = f"{first_index[0]}-csv/{table_index}-data.json"
+                with open(f"{data_dir}/{meta_qa}", "r") as jsonfile:
+                    json_qa = json.load(jsonfile)
+                table_schema = get_table(use_sql)(
+                    table_id, json_qa["metadata"]["title"]
+                )
+                questions = []
+                answers = []
+                for qas in json_qa["examples"]:
+                    questions.append(qas["utterance"])
+                    answers.append(qas["targetValue"])
+                yield *table_schema, questions, answers
+    else:
+        for table_idx in table_list:
+            first_index, table_index = tuple(table_idx.split("/"))
+            table_id = f"{first_index}-csv/{table_index}-clean.html"
+            meta_qa = f"{first_index}-csv/{table_index}-data.json"
+            with open(f"{data_dir}/{meta_qa}", "r") as jsonfile:
+                json_qa = json.load(jsonfile)
+            table_schema = get_table(use_sql)(table_id, json_qa["metadata"]["title"])
+            questions = []
+            answers = []
+            for qas in json_qa["examples"]:
+                questions.append(qas["utterance"])
+                answers.append(qas["targetValue"])
+            yield *table_schema, questions, answers
